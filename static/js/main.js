0fd5c00845864ae3643faae9ba6c11fb89d41d28
montfortkorir <ipkipkorir@gmail.com>
2024-09-03 15:22:25 +0300

    Modify slides

--- static/js/main.js
+++ static/js/main.js
@@ -25,19 +25,18 @@ document.querySelector(".close-menu").addEventListener("click", closeNav);
 Slideshow animation
 **************************************/
 /**Select all images and hold them in a constant variable **/
-const slide_show_images = document.querySelectorAll(".banner .slideshow-img");
+/**Set the delay time and the slide-div counter**/
+const slide_show_div = document.querySelectorAll('.banner .slideshow')
+const next_slide_delay = 4000;
+let current_slide_counter = 0;
 
-/**Set the delay time and the image counter**/
-const next_image_delay = 4000;
-let current_image_counter = 0;
+/**Display the slides one at a time**/
+slide_show_div[current_slide_counter].classList.add('active');//.style.display = block;
 
-/**Display the images one at a time**/
-slide_show_images[current_image_counter].style.opacity = 1;
+setInterval(nextSlide, next_slide_delay);
 
-setInterval(nextImage, next_image_delay);
-
-function nextImage() {
-	slide_show_images[current_image_counter].style.opacity = 0;
-	current_image_counter = (current_image_counter + 1) % slide_show_images.length;
-	slide_show_images[current_image_counter].style.opacity = 1;
+function nextSlide() {
+	slide_show_div[current_slide_counter].classList.remove('active');//style.display = none;
+	current_slide_counter = (current_slide_counter + 1) % slide_show_div.length;
+	slide_show_div[current_slide_counter].classList.add('active');//style.display = block;
 }
